d0ad8c3e50fffa49807521cddfc303a14779c166
manohar252 <manohardhungel@gmail.com>
2025-07-07 19:04:16 +0545

    Updated ShopSection.tsx

---
+++ src/components/ShopSection.tsx
@@ -0,0 +1,8 @@
+export default function ShopSection() {
+  return (
+    <div className="p-4 border rounded-lg bg-white">
+      <h2 className="text-xl font-bold">Shop Plants</h2>
+      <p>Your plant products will go here.</p>
+    </div>
+  );
+}
